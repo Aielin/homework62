9386b698d9a88137b374fe4fc1a1477c9ff5df3b
Aielina <aielina.madambekova@gmail.com>
2024-10-21 14:56:44 +0600

    Add Contacts container and routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home/Home.tsx';
 import About from './containers/About/About.tsx';
 import ToolBar from './components/ToolBar/ToolBar.tsx';
+import Contacts from './containers/Contacts/Contacts.tsx';
 
 
 const App: React.FC = () => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
 
     </>
---
+++ src/containers/Contacts/Contacts.tsx
@@ -0,0 +1,13 @@
+
+
+const Contacts = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Contacts</h1>
+      <p>You can reach me via email: aielina.madambekova@gmail.com</p>
+      <p>Or call me at: +996 503 50-00-19</p>
+    </div>
+  );
+};
+
+export default Contacts;
